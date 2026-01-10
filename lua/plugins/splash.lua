75a5df29858f0907fb224cecd7a4c3c799258795
Timo Wolf <timo.wolf@gmx.ch>
2026-01-10 11:15:01 +0100

    added a different splash screen with better defaults, not customized yet

--- lua/plugins/splash.lua
+++ lua/plugins/splash.lua
@@ -1,7 +1,8 @@
 return {
-    "startup-nvim/startup.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim" },
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
     config = function()
-        require "startup".setup()
-    end
+        require('dashboard').setup {w }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } }
 }
