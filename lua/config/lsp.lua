bd1e6479ef11e6dd7d84ed826f92a97766900574
Xenex <timo.wolf@gmx.ch>
2026-02-08 16:32:25 +0100

    stuff

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -11,24 +11,24 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- Shared on_attach for all servers
 local on_attach = function(client, bufnr)
-    -- Keymaps
-    local map = function(mode, lhs, rhs)
-        vim.keymap.set(mode, lhs, rhs, { buffer = bufnr })
-    end
+  -- Keymaps
+  local map = function(mode, lhs, rhs)
+    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr })
+  end
 
-    -- Indent settings
-    vim.bo[bufnr].tabstop = 4
-    vim.bo[bufnr].shiftwidth = 4
-    vim.bo[bufnr].expandtab = true
-    vim.bo[bufnr].smartindent = true
+  -- Indent settings
+  vim.bo[bufnr].tabstop = 2
+  vim.bo[bufnr].shiftwidth = 2
+  vim.bo[bufnr].expandtab = true
+  vim.bo[bufnr].smartindent = true
 end
 
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
 cmp.event:on(
-    'confirm_done',
-    cmp_autopairs.on_confirm_done()
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
 )
 
 -- LuaSnip
@@ -37,52 +37,52 @@ local luasnip = require("luasnip")
 require("luasnip.loaders.from_vscode").lazy_load()
 
 require("luasnip.loaders.from_lua").lazy_load({
-    paths = vim.fn.stdpath("config") .. "/snippets",
+  paths = vim.fn.stdpath("config") .. "/snippets",
 })
 
 luasnip.config.set_config({
-    history = true,
-    updateevents = "TextChanged,TextChangedI",
+  history = true,
+  updateevents = "TextChanged,TextChangedI",
 })
 
 -- Setup nvim-cmp
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
 
-    mapping = cmp.mapping.preset.insert({
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  mapping = cmp.mapping.preset.insert({
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
 
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-    }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
 
-    sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "path" },
-        { name = "buffer" },
-    },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "path" },
+    { name = "buffer" },
+  },
 })
 
 -- Setup autopairs integration
@@ -91,44 +91,44 @@ cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 -- Mason-LSP setup
 mason_lsp.setup({
-    ensure_installed = {
-        "lua_ls",
-        "clangd",
-        "rust_analyzer",
-        "jsonls",
-        "bashls",
-        "taplo",
-        "marksman",
-    },
-    handlers = {
-        -- default handler
-        function(server)
-            lspconfig[server].setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
-        end,
+  ensure_installed = {
+    "lua_ls",
+    "clangd",
+    "rust_analyzer",
+    "jsonls",
+    "bashls",
+    "taplo",
+    "marksman",
+  },
+  handlers = {
+    -- Lua server override
+    ["lua_ls"] = function()
+      lspconfig.lua_ls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+          },
+        },
+      })
+    end,
 
-        -- Lua server override
-        ["lua_ls"] = function()
-            lspconfig.lua_ls.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        diagnostics = { globals = { "vim" } },
-                        workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-                    },
-                },
-            })
-        end,
-    },
+    -- default handler
+    function(server)
+      lspconfig[server].setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+      })
+    end,
+  },
 })
 
 -- Diagnostic config
 vim.diagnostic.config({
-    virtual_text = true,
-    signs = true,
-    underline = true,
-    update_in_insert = false,
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
 })
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -40,6 +40,23 @@ return {
       }):toggle()
     end
 
+    local opencode = Terminal:new({
+      cmd = "opencode",
+      direction = "float",
+      hidden = true,
+      close_on_exit = true,
+      cwd = vim.fn.getcwd(),
+      float_opts = {
+        border = "rounded",
+        width = math.floor(vim.o.columns * 0.9),
+        height = math.floor(vim.o.lines * 0.9),
+      },
+    })
+
+    vim.keymap.set("n", "<leader>oc", function()
+      opencode:toggle()
+    end, { desc = "OpenCode floating terminal" })
+
     vim.keymap.set("n", "<leader>gg", lazygit_toggle, { desc = "Lazygit (cwd aware)" })
 
     vim.keymap.set("n", "<leader>to", function()
@@ -47,5 +64,9 @@ return {
     end, { desc = "Toggle vertical terminal" })
 
     vim.keymap.set("t", "<C-x>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+    vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w><C-h>]], { desc = "move fokus left" })
+    vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w><C-j>]], { desc = "move fokus down" })
+    vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w><C-k>]], { desc = "move fokus up" })
+    vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w><C-l>]], { desc = "move fokus right" })
   end,
 }
